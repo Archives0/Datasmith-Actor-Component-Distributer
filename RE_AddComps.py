0bac062e2db38c502d49bb345d2362e8985d03d4
Archives0 <evlandesg@gmail.com>
2024-10-31 23:45:56 -0500

    Update

--- RE_AddComps.py
+++ RE_AddComps.py
@@ -6,19 +6,11 @@ withComps = list()
 buildingMeshes = list()
 
 metadataKey = "osm_id"
-tagName = "Building"
 
-clickableComp = u.EditorAssetLibrary.load_blueprint_class("/Game/UI/AC_Clickable")
-dataComp = u.EditorAssetLibrary.load_blueprint_class("/Game/UI/AC_Metadata")
-uiComp = u.EditorAssetLibrary.load_blueprint_class("/Game/UI/AC_3DUI")                              ## Made redundant in new interations of project.
+clickableComp = u.EditorAssetLibrary.load_blueprint_class("/Game/Blueprints/UI/AC_Interactable")
+dataComp = u.EditorAssetLibrary.load_blueprint_class("/Game/Blueprints/UI/AC_Metadata")
 damageComp = u.EditorAssetLibrary.load_blueprint_class("/Game/Blueprints/AC_DamageCalculator")
-fluxData = u.EditorAssetLibrary.load_blueprint_class("/Game/FluidFlux/Environment/Readback/BP_FluxDataComponent")
-
-def AddTag(actor):
-    if(u.Name(tagName) not in actor.tags):
-        aTags = actor.tags
-        aTags.append(u.Name(tagName))
-        actor.tags = aTags
+# fluxData = u.EditorAssetLibrary.load_blueprint_class("/Game/FluidFlux/Environment/Readback")
 
 def FindOSMObjects():
     global allActors
@@ -38,8 +30,6 @@ def FindOSMObjects():
         buildingMeshes.append(owner.get_attached_actors()[0])           ## Fill list with only meshes attached to actors.
 
     for mesh in buildingMeshes:
-        AddTag(mesh)
-
         if(mesh.get_component_by_class(clickableComp)):                 ## If clickable component is present.
             withComps.append(mesh)
         else:
@@ -64,7 +54,7 @@ def AddComps():
 
             for mesh in noComps:                                                ## Add components if mesh does not have base clickable component
                 if slowTask.should_cancel():
-                    print("Task canceled")
+                    print("Task cancelled")
                     break
 
                 slowTask.enter_progress_frame(1, "Adding actor components..." + str(counter) + " / " + str(numTasks))
@@ -73,9 +63,7 @@ def AddComps():
 
                 clickSub = soSub.add_new_subobject(u.AddNewSubobjectParams(parent_handle=rootSub, new_class=clickableComp))
                 dataSub = soSub.add_new_subobject(u.AddNewSubobjectParams(parent_handle=rootSub, new_class=dataComp))
-                uiSub = soSub.add_new_subobject(u.AddNewSubobjectParams(parent_handle=rootSub, new_class=uiComp))
                 damSub = soSub.add_new_subobject(u.AddNewSubobjectParams(parent_handle=rootSub, new_class=damageComp))
-                fluxSub = soSub.add_new_subobject(u.AddNewSubobjectParams(parent_handle=rootSub, new_class=fluxData))
 
                 withComps.append(mesh)
                 counter += 1
